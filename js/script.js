2f2a1f226973429ae5fd76857b3d3a351b7f9780
Karol Kowalczyk <KarolKowalczykPL@Gmail.com>
2024-05-11 21:46:35 +0200

    added a btn to increase the amount of the pokemons and displays them in the body

--- js/script.js
+++ js/script.js
@@ -1,29 +1,51 @@
-let maxPokemons = 20;
-const POKE_API = "https://pokeapi.co/api/v2/pokemon?limit=" + maxPokemons;
+const POKE_API = "https://pokeapi.co/api/v2/pokemon?limit=10500&offset=14";
+
+let pokemons = [];
+let currentOffset = 0;
 
 async function loadData() {
   let resp = await fetch(POKE_API);
   let respAsJson = await resp.json();
   let respAsJsonResults = respAsJson.results;
-  console.log(respAsJsonResults);
-  loadCards(respAsJsonResults);
+  showPokemonCards(respAsJsonResults);
 }
 
-let positions = [];
+function showPokemonCards(respAsJsonResults) {
+  for (let index = 0; index < 20; index++) {
+    pokemons.push(respAsJsonResults[index]);
+    addPokemons(index);
+  }
+}
+
+function addPokemons(index) {
+  let card = document.getElementById("card-field");
+  card.innerHTML += /*html*/ `
+    <div class="card"><p class="number">${pokemons.length}</p>
+            <div class="name">${pokemons[index].name}</div>
+            <img src="" alt="" />
+            <div class="attributes"></div>
+            </div>`;
+}
+
+function increasePositions() {
+  currentOffset += 5;
+  fetchMorePokemon();
+}
+
+async function fetchMorePokemon() {
+  let resp = await fetch(`${POKE_API}&offset=${currentOffset}`);
+  let respAsJson = await resp.json();
+  let respAsJsonResults = respAsJson.results;
+  showMorePokemonCards(respAsJsonResults);
+}
+
+function showMorePokemonCards(respAsJsonResults) {
+  for (let index = 0; index < 10; index++) {
+    pokemons.push(respAsJsonResults[index]);
+    addPokemons(index + 20); // Start adding new Pokemon from index 20
+  }
+}
 
 function reloadSite() {
     location.reload();
-}
-
-function loadCards(respAsJsonResults) {
-    for (let index = 0; index < maxPokemons; index++) {
-        let card = document.getElementById("card-field");
-        positions.push(index);
-
-        card.innerHTML += /*html*/ `<div class="card"><p class="number">${positions[index]+1}</p>
-        <div class="name">${respAsJsonResults[index].name}</div>
-        <img src="" alt="" />
-        <div class="attributes"></div>
-        </div>`;
-    }
-}
+  }
