c2a283c3183b5c916a526711415312422b2cf58f
Karol Kowalczyk <KarolKowalczykPL@Gmail.com>
2024-05-11 20:18:32 +0200

    I merged the remote storage into the script.js.

--- js/remoteStorage.js
+++
@@ -1,9 +0,0 @@
-const POKE_API = 'https://pokeapi.co/api/v2/pokemon?limit=500';
-
-async function loadData() {
-  let resp = await fetch(POKE_API);
-  let respAsJson = await resp.json();
-  let respAsJsonResults = respAsJson.results;
-  console.log(respAsJsonResults);
-  loadCards(respAsJsonResults);
-}
--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
+let maxPokemons = 20;
+const POKE_API = "https://pokeapi.co/api/v2/pokemon?limit=" + maxPokemons;
+
+async function loadData() {
+  let resp = await fetch(POKE_API);
+  let respAsJson = await resp.json();
+  let respAsJsonResults = respAsJson.results;
+  console.log(respAsJsonResults);
+  loadCards(respAsJsonResults);
+}
+
 let positions = [];
 
 function reloadSite() {
@@ -5,13 +16,14 @@ function reloadSite() {
 }
 
 function loadCards(respAsJsonResults) {
-    for (let index = 0; index < 10; index++) {
-        let card = document.getElementById("card");
+    for (let index = 0; index < maxPokemons; index++) {
+        let card = document.getElementById("card-field");
         positions.push(index);
 
-        card.innerHTML += /*html*/ `<p class="number">${positions[index]+1}</p>
+        card.innerHTML += /*html*/ `<div class="card"><p class="number">${positions[index]+1}</p>
         <div class="name">${respAsJsonResults[index].name}</div>
         <img src="" alt="" />
-        <div class="attributes"></div>`;
+        <div class="attributes"></div>
+        </div>`;
     }
 }
